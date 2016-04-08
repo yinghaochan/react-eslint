48d303d62ba7177988fd8a3aac4a940679f6a1c2
Ying Hao Chan <yinghaochan@Yings-MacBook-Pro-2.local>
2016-04-08 10:26:23 -0700

    added more react specific rules

--- rules.js
+++ rules.js
@@ -1,33 +1,57 @@
 module.exports = {
-    'semi'                : 'off',
-    'indent'              : ['error', 4],
+
+    'semi'                      : 'off',
+    
+    'indent'                    : ['error', 4],
     
     // only dangle comments with multiline objs
-    'comma-dangle'        : ['warn', 'always-multiline'],
+    'comma-dangle'              : ['warn', 'always-multiline'],
     
     // Errs if you need to use quotes, but ignores other cases.
-    'quote-props'         : ['error', 'as-needed', { 
+    'quote-props'               : ['error', 'as-needed', { 
         'keywords' : false, 
         unnecessary: false, 
     }],
     
     // generous use of spaces is nice. 
     // finer grained control gets weird
-    'key-spacing'         : ['off', {
+    'key-spacing'               : ['off', {
         beforeColon: false, 
         afterColon : true, 
         mode       : 'minimum',
     }],
     
-    'no-trailing-spaces'  : 'off',
+    'no-trailing-spaces'        : 'off',
     
     // generous object formatting {sometimes works best}
-    'object-curly-spacing': 'off',
+    'object-curly-spacing'      : 'off',
     
     // allows var for backwards compatibility
-    'no-var'              : 'off',
-
+    'no-var'                    : 'off',
+    
     // allows anon functions
-    'func-names'          : 'off',
- 
+    'func-names'                : 'off',
+    
+    // allows string concat
+    'prefer-template'           : 'off',
+
+    // allows '///'
+    'spaced-comment'            : ['warn', {markers: ['/']}],
+
+
+    ////////////////////////
+    // REACT CUSTOM RULES //
+    ////////////////////////
+    
+    // allow React.createClass()
+    'react/prefer-es6-class'    : 'off',
+    
+    // allow import { x } from 'react'
+    'react/react-in-jsx-scope'  : 'off',
+
+    // warn for prop-types
+    'react/prop-types'          : 'warn',
+
+
+
 }
